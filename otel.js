43f86179a1d64f9ac48293b5a7681926191d1900
Valber Junior <84424883+ValberJunior@users.noreply.github.com>
2025-01-12 09:01:39 -0300

    Analysis not available.: Update otel.js

---
+++ otel.js
@@ -0,0 +1,9 @@
+// otel.js
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+
+const sdk = new NodeSDK({
+  instrumentations: [getNodeAutoInstrumentations()]
+});
+
+sdk.start();
